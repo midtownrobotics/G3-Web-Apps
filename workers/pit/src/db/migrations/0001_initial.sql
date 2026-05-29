53415ed0820047d121406c130abd556facfd5a2e
Gray <me@grayjn.com>
2026-05-29 00:58:06 -0400

    fix(pit): add navbar link

--- workers/pit/src/db/migrations/0001_initial.sql
+++ workers/pit/src/db/migrations/0001_initial.sql
@@ -8,7 +8,6 @@ CREATE TABLE checklist_lists (
 CREATE TABLE checklist_items (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   list_id INTEGER NOT NULL REFERENCES checklist_lists(id),
-  "index" INTEGER NOT NULL DEFAULT 0,
   name TEXT NOT NULL,
   description TEXT,
   created_at INTEGER NOT NULL
