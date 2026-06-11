323c7077f7568e2509f5726e5947e3b08e0fbc35
Gray <me@grayjn.com>
2026-06-10 20:22:54 -0400

    add skill tree

--- apps/web/src/plugins/members/members-page.tsx
+++ apps/web/src/plugins/members/members-page.tsx
@@ -6,9 +6,13 @@ import {
   FaInstagram,
   FaSlack,
   FaToolbox,
+  FaTree,
   FaTrophy,
   FaUserShield,
 } from "react-icons/fa";
+import { FaTreeCity } from "react-icons/fa6";
+import { GiBranchArrow } from "react-icons/gi";
+import { LuGitBranch } from "react-icons/lu";
 
 type App = {
   label: string;
@@ -43,6 +47,12 @@ const APPS: App[] = [
     icon: FaHardHat,
     bg: "bg-yellow-600",
   },
+  {
+    label: "Skill Tree",
+    href: "https://skilltree.g3robotics.com",
+    icon: LuGitBranch,
+    bg: "bg-amber-900",
+  },
   // {
   //   label: "Scouting",
   //   href: "https://scouting.g3robotics.com",
@@ -67,7 +77,7 @@ const APPS: App[] = [
     label: "Statbotics",
     href: "https://www.statbotics.io/team/1648",
     icon: FaChartLine,
-    bg: "bg-emerald-600",
+    bg: "bg-gray-400",
     external: true,
   },
   {
